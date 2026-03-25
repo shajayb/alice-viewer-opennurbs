65a30d51348f09cfc71badb0a82092daffd56793
shajay <shajay.bhooshan@gmail.com>
2026-03-25 19:05:35 +0000

    Refactor: Ground-up Core Profile reboot with Maya CAD camera, Ninja, and Clang

--- include/AliceViewer.h
+++ include/AliceViewer.h
@@ -1,36 +1,136 @@
 #pragma once
 
-#include <string>
-#include <GLFW/glfw3.h>
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include <cmath>
 
-// Forward declarations of user-defined functions
-void setup();
-void update();
-void draw();
-void mouse(int button, int action, double xpos, double ypos);
+#pragma pack(push, 1)
 
-class AliceViewer {
+struct V3
+{
+    float x, y, z;
+};
+
+struct M4
+{
+    float m[16];
+};
+
+struct ArcballCamera
+{
+    V3 focusPoint;
+    float distance;
+    float yaw;
+    float pitch;
+    
+    float lastMouseX;
+    float lastMouseY;
+
+    M4 getViewMatrix() const;
+};
+
+#pragma pack(pop)
+
+inline V3 sub(V3 a, V3 b)
+{
+    return { a.x - b.x, a.y - b.y, a.z - b.z };
+}
+
+inline V3 add(V3 a, V3 b)
+{
+    return { a.x + b.x, a.y + b.y, a.z + b.z };
+}
+
+inline V3 mul(V3 v, float s)
+{
+    return { v.x * s, v.y * s, v.z * s };
+}
+
+inline float dot(V3 a, V3 b)
+{
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+inline V3 crs(V3 a, V3 b)
+{
+    return { a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x };
+}
+
+inline V3 nrm(V3 v)
+{
+    float l = sqrtf(dot(v, v));
+    if (l > 0.0f)
+    {
+        return mul(v, 1.0f / l);
+    }
+    return v;
+}
+
+inline M4 perspective(float fov, float asp, float n, float f)
+{
+    float tn = tanf(fov * 0.5f);
+    M4 r = { 0 };
+    r.m[0] = 1.0f / (asp * tn);
+    r.m[5] = 1.0f / tn;
+    r.m[10] = -(f + n) / (f - n);
+    r.m[11] = -1.0f;
+    r.m[14] = -(2.0f * f * n) / (f - n);
+    return r;
+}
+
+inline M4 lookAt(V3 eye, V3 target, V3 up)
+{
+    V3 f = nrm(sub(target, eye));
+    V3 s = nrm(crs(f, up));
+    V3 v = crs(s, f);
+    
+    M4 r = { 0 };
+    r.m[0] = s.x;
+    r.m[4] = s.y;
+    r.m[8] = s.z;
+    
+    r.m[1] = v.x;
+    r.m[5] = v.y;
+    r.m[9] = v.z;
+    
+    r.m[2] = -f.x;
+    r.m[6] = -f.y;
+    r.m[10] = -f.z;
+    
+    r.m[12] = -dot(s, eye);
+    r.m[13] = -dot(v, eye);
+    r.m[14] = dot(f, eye);
+    r.m[15] = 1.0f;
+    
+    return r;
+}
+
+inline M4 mult(M4 a, M4 b)
+{
+    M4 r = { 0 };
+    for (int j = 0; j < 4; ++j)
+    {
+        for (int i = 0; i < 4; ++i)
+        {
+            for (int k = 0; k < 4; ++k)
+            {
+                r.m[j * 4 + i] += a.m[k * 4 + i] * b.m[j * 4 + k];
+            }
+        }
+    }
+    return r;
+}
+
+struct GLFWwindow;
+
+class AliceViewer
+{
 public:
-    AliceViewer(const std::string& title = "AliceViewer", int width = 1280, int height = 720);
-    ~AliceViewer();
+    GLFWwindow* window;
+    ArcballCamera camera;
+    unsigned int shaderProgram;
+    unsigned int vao, vbo;
+    int gridVertexCount;
 
-    bool init();
+    void init();
     void run();
-
-private:
-    static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-    static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
-    static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
-    static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-    static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
-
-    void processInput();
-
-    GLFWwindow* m_window;
-    std::string m_title;
-    int m_width;
-    int m_height;
+    void cleanup();
 };
--- src/AliceViewer.cpp
+++ src/AliceViewer.cpp
@@ -1,119 +1,233 @@
 #include "AliceViewer.h"
-#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <vector>
+#include <algorithm>
 
-AliceViewer::AliceViewer(const std::string& title, int width, int height)
-    : m_title(title), m_width(width), m_height(height), m_window(nullptr) {}
+static const char* vertexShaderSource = R"(#version 410 core
+layout(location = 0) in vec3 position;
+uniform mat4 mvp;
+void main()
+{
+    gl_Position = mvp * vec4(position, 1.0);
+}
+)";
 
-AliceViewer::~AliceViewer() {
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
+static const char* fragmentShaderSource = R"(#version 410 core
+out vec4 fragColor;
+void main()
+{
+    fragColor = vec4(0.4, 0.4, 0.4, 1.0);
+}
+)";
 
-    if (m_window) {
-        glfwDestroyWindow(m_window);
-    }
-    glfwTerminate();
+M4 ArcballCamera::getViewMatrix() const
+{
+    float cosPitch = cosf(pitch);
+    float sinPitch = sinf(pitch);
+    float cosYaw = cosf(yaw);
+    float sinYaw = sinf(yaw);
+
+    V3 eye;
+    eye.x = focusPoint.x + distance * cosPitch * sinYaw;
+    eye.y = focusPoint.y - distance * cosPitch * cosYaw;
+    eye.z = focusPoint.z + distance * sinPitch;
+
+    return lookAt(eye, focusPoint, { 0.0f, 0.0f, 1.0f });
 }
 
-bool AliceViewer::init() {
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return false;
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+void AliceViewer::init()
+{
+    glfwInit();
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), nullptr, nullptr);
-    if (!m_window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return false;
-    }
-
-    glfwMakeContextCurrent(m_window);
-    glfwSwapInterval(1); // Enable v-sync
-
-    // Initialize ImGui
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    
+    window = glfwCreateWindow(1280, 720, "AliceViewer - CAD Engine", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 410 core");
 
-    ImGui::StyleColorsDark();
+    camera.focusPoint = { 0.0f, 0.0f, 0.0f };
+    camera.distance = 20.0f;
+    camera.yaw = 0.785f; // 45 degrees
+    camera.pitch = 0.6f;
+    camera.lastMouseX = 0.0f;
+    camera.lastMouseY = 0.0f;
 
-    ImGui_ImplGlfw_InitForOpenGL(m_window, true);
-    ImGui_ImplOpenGL3_Init("#version 150");
+    unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vs);
+    
+    unsigned int fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fs);
+    
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
+    glLinkProgram(shaderProgram);
+    
+    glDeleteShader(vs);
+    glDeleteShader(fs);
 
-    // Set callbacks
-    glfwSetWindowUserPointer(m_window, this);
-    glfwSetFramebufferSizeCallback(m_window, framebuffer_size_callback);
-    glfwSetMouseButtonCallback(m_window, mouse_button_callback);
-    glfwSetCursorPosCallback(m_window, cursor_position_callback);
-    glfwSetScrollCallback(m_window, scroll_callback);
-    glfwSetKeyCallback(m_window, key_callback);
+    std::vector<float> gridData;
+    for (int i = -20; i <= 20; ++i)
+    {
+        gridData.push_back((float)i); gridData.push_back(-20.0f); gridData.push_back(0.0f);
+        gridData.push_back((float)i); gridData.push_back(20.0f); gridData.push_back(0.0f);
+        
+        gridData.push_back(-20.0f); gridData.push_back((float)i); gridData.push_back(0.0f);
+        gridData.push_back(20.0f); gridData.push_back((float)i); gridData.push_back(0.0f);
+    }
+    gridVertexCount = (int)(gridData.size() / 3);
 
-    return true;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, gridData.size() * sizeof(float), gridData.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 }
 
-void AliceViewer::run() {
-    setup();
-
-    while (!glfwWindowShouldClose(m_window)) {
+void AliceViewer::run()
+{
+    while (!glfwWindowShouldClose(window))
+    {
         glfwPollEvents();
-        processInput();
-
-        update();
-
-        // Start ImGui frame
+        
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // Clear screen
-        int display_w, display_h;
-        glfwGetFramebufferSize(m_window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        ImGuiIO& io = ImGui::GetIO();
+        
+        double mouseX, mouseY;
+        glfwGetCursorPos(window, &mouseX, &mouseY);
+        float dx = (float)mouseX - camera.lastMouseX;
+        float dy = (float)mouseY - camera.lastMouseY;
+        camera.lastMouseX = (float)mouseX;
+        camera.lastMouseY = (float)mouseY;
 
-        draw();
+        if (!io.WantCaptureMouse)
+        {
+            // Left Mouse Button: Orbit / Tumble
+            if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+            {
+                camera.yaw -= dx * 0.005f;
+                camera.pitch += dy * 0.005f;
+            }
+            // Middle Mouse Button: Track / Pan
+            else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE) == GLFW_PRESS)
+            {
+                float cosPitch = cosf(camera.pitch);
+                float sinPitch = sinf(camera.pitch);
+                float cosYaw = cosf(camera.yaw);
+                float sinYaw = sinf(camera.yaw);
 
-        // Render ImGui
+                V3 eye;
+                eye.x = camera.focusPoint.x + camera.distance * cosPitch * sinYaw;
+                eye.y = camera.focusPoint.y - camera.distance * cosPitch * cosYaw;
+                eye.z = camera.focusPoint.z + camera.distance * sinPitch;
+
+                V3 forward = nrm(sub(camera.focusPoint, eye));
+                V3 worldUp = { 0.0f, 0.0f, 1.0f };
+                V3 right = nrm(crs(forward, worldUp));
+                V3 up = crs(right, forward);
+
+                float panScale = camera.distance * 0.001f;
+                camera.focusPoint = add(camera.focusPoint, mul(right, -dx * panScale));
+                camera.focusPoint = add(camera.focusPoint, mul(up, dy * panScale));
+            }
+            // Right Mouse Button: Dolly / Smooth Zoom
+            else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
+            {
+                camera.distance += dy * 0.1f;
+            }
+
+            // Scroll Wheel: Stepped Zoom
+            if (io.MouseWheel != 0.0f)
+            {
+                if (io.MouseWheel > 0.0f)
+                {
+                    camera.distance *= 0.9f;
+                }
+                else
+                {
+                    camera.distance *= 1.1f;
+                }
+            }
+        }
+
+        // Constraints
+        camera.pitch = std::max(-1.55f, std::min(1.55f, camera.pitch)); // approx -89 to 89 degrees
+        if (camera.distance < 0.1f)
+        {
+            camera.distance = 0.1f;
+        }
+
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        if (width <= 0 || height <= 0)
+        {
+            continue;
+        }
+        
+        glViewport(0, 0, width, height);
+        glClearColor(0.85f, 0.85f, 0.85f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+
+        float aspectRatio = (float)width / (float)height;
+        M4 projection = perspective(0.785f, aspectRatio, 0.1f, 1000.0f);
+        M4 view = camera.getViewMatrix();
+        M4 mvp = mult(projection, view);
+
+        glUseProgram(shaderProgram);
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "mvp"), 1, GL_FALSE, mvp.m);
+        
+        glBindVertexArray(vao);
+        glDrawArrays(GL_LINES, 0, gridVertexCount);
+
+        ImGui::Begin("Camera Debug");
+        ImGui::Text("Yaw: %.3f", camera.yaw);
+        ImGui::Text("Pitch: %.3f", camera.pitch);
+        ImGui::Text("Distance: %.3f", camera.distance);
+        ImGui::Text("Focus: %.2f, %.2f, %.2f", camera.focusPoint.x, camera.focusPoint.y, camera.focusPoint.z);
+        if (ImGui::Button("Reset Camera"))
+        {
+            camera.focusPoint = { 0.0f, 0.0f, 0.0f };
+            camera.distance = 20.0f;
+            camera.yaw = 0.785f;
+            camera.pitch = 0.6f;
+        }
+        ImGui::End();
+
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(m_window);
+        
+        glfwSwapBuffers(window);
     }
 }
 
-void AliceViewer::processInput() {
-    if (glfwGetKey(m_window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(m_window, true);
-}
-
-void AliceViewer::framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
-
-void AliceViewer::mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
-    double xpos, ypos;
-    glfwGetCursorPos(window, &xpos, &ypos);
-    mouse(button, action, xpos, ypos);
-}
-
-void AliceViewer::cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
-    // Optionally handle motion here if needed, or pass to mouse() with a custom action code
-}
-
-void AliceViewer::scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
-    // Handle scroll
-}
-
-void AliceViewer::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    // Handle keys
+void AliceViewer::cleanup()
+{
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteProgram(shaderProgram);
+    
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }

23416f9e7ddfcb7a054786164a63e1bfb9ad3558
Shajay Bhooshan <shajay.bhooshan@zaha-hadid.com>
2026-04-01 15:29:42 +0100

    ci: fix portfile corruption and vcpkg git worktree

--- vcpkg-overlay-ports/ports/opennurbs/portfile.cmake
+++ vcpkg-overlay-ports/ports/opennurbs/portfile.cmake
@@ -1,4 +1,4 @@
-﻿vcpkg_from_github(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mcneel/opennurbs
     REF v8.24.25281.15001
@@ -7,7 +7,7 @@
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "\"
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DOPENNURBS_BUILD_EXAMPLES=OFF
 )
@@ -15,13 +15,13 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 # Ensure all headers are installed to include/ root
-file(GLOB HEADERS "\/*.h")
-file(INSTALL \ DESTINATION "\/include")
+file(GLOB HEADERS "${SOURCE_PATH}/*.h")
+file(INSTALL ${HEADERS} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 # Clean up any nested include folders
-file(REMOVE_RECURSE "\/include/OpenNURBS")
-file(REMOVE_RECURSE "\/include/opennurbsStatic")
-file(REMOVE_RECURSE "\/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/OpenNURBS")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/opennurbsStatic")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # Manual config file with transitive dependencies
 set(LIB_NAME "opennurbsStatic")
@@ -35,24 +35,24 @@ endif()
 set(CONFIG_CONTENT "
 add_library(opennurbs::opennurbs STATIC IMPORTED)
 set_target_properties(opennurbs::opennurbs PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES \"\\\/../../include\"
-    IMPORTED_LOCATION \"\\\/../../lib/\\\"
+    INTERFACE_INCLUDE_DIRECTORIES \"\${CMAKE_CURRENT_LIST_DIR}/../../include\"
+    IMPORTED_LOCATION \"\${CMAKE_CURRENT_LIST_DIR}/../../lib/${LIB_NAME}${LIB_EXT}\"
 ")
 
 if(WIN32)
-    string(APPEND CONFIG_CONTENT "    INTERFACE_LINK_LIBRARIES \"shlwapi.lib;\\\/../../lib/zlib.lib\"\n")
+    string(APPEND CONFIG_CONTENT "    INTERFACE_LINK_LIBRARIES \"shlwapi.lib;\${CMAKE_CURRENT_LIST_DIR}/../../lib/zlib.lib\"\n")
 else()
     string(APPEND CONFIG_CONTENT "    INTERFACE_LINK_LIBRARIES \"z\"\n")
 endif()
 
 string(APPEND CONFIG_CONTENT ")\n")
 
-string(APPEND CONFIG_CONTENT "if(EXISTS \"\\\/../../debug/lib/\\\")\n")
+string(APPEND CONFIG_CONTENT "if(EXISTS \"\${CMAKE_CURRENT_LIST_DIR}/../../debug/lib/${LIB_NAME}${LIB_EXT}\")\n")
 string(APPEND CONFIG_CONTENT "    set_target_properties(opennurbs::opennurbs PROPERTIES\n")
-string(APPEND CONFIG_CONTENT "        IMPORTED_LOCATION_DEBUG \"\\\/../../debug/lib/\\\"\n")
+string(APPEND CONFIG_CONTENT "        IMPORTED_LOCATION_DEBUG \"\${CMAKE_CURRENT_LIST_DIR}/../../debug/lib/${LIB_NAME}${LIB_EXT}\"\n")
 
 if(WIN32)
-    string(APPEND CONFIG_CONTENT "        INTERFACE_LINK_LIBRARIES_DEBUG \"shlwapi.lib;\\\/../../debug/lib/zlib.lib\"\n")
+    string(APPEND CONFIG_CONTENT "        INTERFACE_LINK_LIBRARIES_DEBUG \"shlwapi.lib;\${CMAKE_CURRENT_LIST_DIR}/../../debug/lib/zlib.lib\"\n")
 else()
     string(APPEND CONFIG_CONTENT "        INTERFACE_LINK_LIBRARIES_DEBUG \"z\"\n")
 endif()
@@ -60,11 +60,11 @@ endif()
 string(APPEND CONFIG_CONTENT "    )\n")
 string(APPEND CONFIG_CONTENT "endif()\n")
 
-file(WRITE "\/share/opennurbs/opennurbsConfig.cmake" "\")
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/opennurbs/opennurbsConfig.cmake" "${CONFIG_CONTENT}")
 
 # License
-if(EXISTS "\/license.txt")
-    file(INSTALL "\/license.txt" DESTINATION "\/share/\" RENAME copyright)
-elseif(EXISTS "\/LICENSE")
-    file(INSTALL "\/LICENSE" DESTINATION "\/share/\" RENAME copyright) 
+if(EXISTS "${SOURCE_PATH}/license.txt")
+    file(INSTALL "${SOURCE_PATH}/license.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+elseif(EXISTS "${SOURCE_PATH}/LICENSE")
+    file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright) 
 endif()

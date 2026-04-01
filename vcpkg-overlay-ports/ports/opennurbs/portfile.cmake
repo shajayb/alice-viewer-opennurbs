e6aa75f366f564ac379eea07631bd956e87287ee
Shajay Bhooshan <shajay.bhooshan@zaha-hadid.com>
2026-04-01 15:12:45 +0100

    ci: enforce public baseline and flatten overlay directory structure

---
+++ vcpkg-overlay-ports/ports/opennurbs/portfile.cmake
@@ -0,0 +1,47 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO mcneel/opennurbs
+    REF v8.24.25281.15001
+    SHA512 E115F27ABC40861A00EF445C65D9A7EC8B6E7772742C30A31BF7F0D4AC54B557C4B58B84BFE13592BE46B64711E1CA3AC016A61FD32A31EF6135702FCFD51D2C
+    HEAD_REF 8.x
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DOPENNURBS_BUILD_EXAMPLES=OFF
+)
+
+vcpkg_cmake_install()
+
+# Ensure all headers are installed to include/ root
+file(GLOB HEADERS "${SOURCE_PATH}/*.h")
+file(INSTALL ${HEADERS} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+# Clean up any nested include folders
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/OpenNURBS")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/opennurbsStatic")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+# Manual config file with transitive dependencies
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/opennurbs/opennurbsConfig.cmake" "
+add_library(opennurbs::opennurbs STATIC IMPORTED)
+set_target_properties(opennurbs::opennurbs PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES \"\${CMAKE_CURRENT_LIST_DIR}/../../include\"
+    IMPORTED_LOCATION \"\${CMAKE_CURRENT_LIST_DIR}/../../lib/opennurbsStatic.lib\"
+    INTERFACE_LINK_LIBRARIES \"shlwapi.lib;\${CMAKE_CURRENT_LIST_DIR}/../../lib/zlib.lib\"
+)
+if(EXISTS \"\${CMAKE_CURRENT_LIST_DIR}/../../debug/lib/opennurbsStatic.lib\")
+    set_target_properties(opennurbs::opennurbs PROPERTIES
+        IMPORTED_LOCATION_DEBUG \"\${CMAKE_CURRENT_LIST_DIR}/../../debug/lib/opennurbsStatic.lib\"
+        INTERFACE_LINK_LIBRARIES_DEBUG \"shlwapi.lib;\${CMAKE_CURRENT_LIST_DIR}/../../debug/lib/zlib.lib\"
+    )
+endif()
+")
+
+# License
+if(EXISTS "${SOURCE_PATH}/license.txt")
+    file(INSTALL "${SOURCE_PATH}/license.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+elseif(EXISTS "${SOURCE_PATH}/LICENSE")
+    file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+endif()
